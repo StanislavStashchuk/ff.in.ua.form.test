4b73b99e2c9aee930bf769f5e8577983935daa76
Stanislav Stashchuk <stashchuk_stanislav@ukr.net>
2022-03-30 01:24:56 +0300

    implementing email

--- src/javascripts/form.js
+++ src/javascripts/form.js
@@ -6,9 +6,18 @@ const email = document.querySelector('[name="email"]');
 const message = document.querySelector('[name="message"]');
 
 document.querySelector('[type="submit"]')
-    .addEventListener('click',(event)=> {
-        event.preventDefault();
+  .addEventListener('click', (event) => {
+    event.preventDefault();
 
-        console.log(name.value, email.value, message.value );
+    console.log(name.value, email.value, message.value);
+
+    $.ajax({
+      method: 'POST',
+      url: 'https://dynamic-pixie-49c48e.netlify.app/.netlify/functions/email',
+      data: JSON.stringify({
+        name: name.value,
+        email: email.value,
+        message: message.value
+      })
     });
-    
+  });
