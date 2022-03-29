b98662117c4be43d4851acfefe614fad4150a968
Stanislav Stashchuk <stashchuk_stanislav@ukr.net>
2022-03-30 01:09:36 +0300

    implementing email

--- src/javascripts/form.js
+++ src/javascripts/form.js
@@ -4,3 +4,11 @@ import $ from 'jquery';
 const name = document.querySelector('[name="name"]');
 const email = document.querySelector('[name="email"]');
 const message = document.querySelector('[name="message"]');
+
+document.querySelector('[type="submit"]')
+    .addEventListener('click',(event)=> {
+        event.preventDefault();
+
+        console.log(name.value, email.value, message.value );
+    });
+    
